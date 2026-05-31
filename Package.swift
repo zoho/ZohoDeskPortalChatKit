da873ac31bab2fc0a79c8dac44ec25a7382369d1
Vinoth N <vinoth.nv@zohocorp.com>
2026-05-31 18:17:17 +0530

    4.6.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = Version("4.6.2")
+let version = Version("4.6.3")
 let package = Package(
     name: "ZohoDeskPortalChatKit",
     platforms: [
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalChatKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalChatKit/\(version.description)/ZohoDeskPortalChatKit.zip", checksum: "3d78f2d67a31c64aff55902dd16c6859cb290ac1a06174184ad6e3c3670344fc"),
+        .binaryTarget(name: "ZohoDeskPortalChatKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalChatKit/\(version.description)/ZohoDeskPortalChatKit.zip", checksum: "6327a2897d5b4e27756d8f22b189a3f319504b85e6e5bcb5a70f0cc98c9628b3"),
         
-        .binaryTarget(name: "ZDChat", url: "https://maven.zohodl.com/ZohoDesk/ZohoGC/iOS/2.6.0/ZDChat.zip", checksum: "5fb8f1ecc1acc6b24e27c66f880e73d86040681dbeb4b601effda3b03db2f959"),
+        .binaryTarget(name: "ZDChat", url: "https://maven.zohodl.com/ZohoDesk/ZohoGC/iOS/2.6.2/ZDChat.zip", checksum: "f3e55c890997411dce2795339662156ff661f3bc3ac2ed0eee89e3263ba1b807"),
         
         .binaryTarget(name: "ZohoPubSubSDK", url: "https://maven.zohodl.com/ZohoDesk/ZohoPubSubSDK/iOS/1.3.0/framework/ZohoPubSubSDK.zip", checksum: "7bac396e2ae30ea50186aed8f7d2bbfff23f71a5974c38bd05e372116f65912f"),
         
-        .binaryTarget(name: "ZDOneChatSDK", url: "https://maven.zohodl.com/ZohoDesk/ZohoGC/iOS/1.5.0/ZDOneChatSDK.zip", checksum: "039de23a2c1517dd63f8a28e0b5badaba395a1aa6a86f40d4909bf618884c541"),
+        .binaryTarget(name: "ZDOneChatSDK", url: "https://maven.zohodl.com/ZohoDesk/ZohoGC/iOS/1.5.2/ZDOneChatSDK.zip", checksum: "0f02f8bd1b47014f7d79350c9a5e8a1a8617406861542a8a0122846e65e86a51"),
                 
         .target(
             name: "ZohoDeskPortalChatKitPackage",
